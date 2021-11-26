ade4fa705c9f02906b4a1caccd6bf03f4d8990f8
IHoneyBunnyI <kasim.baybikov@gmail.com>
2021-11-26 23:12:58 +0300

    small fix

--- srcs/color.c
+++ srcs/color.c
@@ -15,21 +15,21 @@ int	get_light(int start, int end, double percentage)
 	return ((int)((1 - percentage) * start + percentage * end));
 }
 
-int	get_color(t_point current, t_point start, t_point end, t_point delta)
+int	get_color(t_point current, t_point s, t_point e, t_point delta)
 {
 	int		red;
 	int		green;
 	int		blue;
 	double	percentage;
 
-	if (current.color == end.color)
+	if (current.color == e.color)
 		return (current.color);
 	if (delta.x > delta.y)
-		percentage = percent(start.x, end.x, current.x);
+		percentage = percent(s.x, e.x, current.x);
 	else
-		percentage = percent(start.y, end.y, current.y);
-	red = get_light((start.color >> 16) & 0xFF, (end.color >> 16) & 0xFF, percentage);
-	green = get_light((start.color >> 8) & 0xFF, (end.color >> 8) & 0xFF, percentage);
-	blue = get_light(start.color & 0xFF, end.color & 0xFF, percentage);
+		percentage = percent(s.y, e.y, current.y);
+	red = get_light((s.color >> 16) & 0xFF, (e.color >> 16) & 0xFF, percentage);
+	green = get_light((s.color >> 8) & 0xFF, (e.color >> 8) & 0xFF, percentage);
+	blue = get_light(s.color & 0xFF, e.color & 0xFF, percentage);
 	return ((red << 16) | (green << 8) | blue);
 }

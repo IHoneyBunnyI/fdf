435b5cc2de84c148186d98572ed960028e8b02f7
IHoneyBunnyI <kasim.baybikov@gmail.com>
2022-01-11 17:11:21 +0300

    gnl norm header

--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -1,16 +1,15 @@
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFFER_SIZE 16
+# define R =
+# define A ?
+# define B :
 
-#define BUFFER_SIZE 16
-#define R =
-#define A ?
-#define B :
-
-char		*ft_strjoin(char *s1, char *s2);
-int			find_n(char *cache);
-char		*ft_strchr(const char *s, int c);
-int			ft_strlen(char *s);
-char		*ft_strdup(char *s);
-int				get_next_line(int fd, char **line);
+char	*ft_strjoin(char *s1, char *s2);
+int		find_n(char *cache);
+char	*ft_strchr(const char *s, int c);
+int		ft_strlen(char *s);
+char	*ft_strdup(char *s);
+int		get_next_line(int fd, char **line);
 
 #endif
